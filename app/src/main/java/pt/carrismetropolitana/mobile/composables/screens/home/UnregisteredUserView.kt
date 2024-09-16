14b45147ff1454c505704c537bbd392c28a47375
joaodcp <67809513+joaodcp@users.noreply.github.com>
2024-09-16 17:12:16 +0100

    feat: add "Add first favorite" button to UnregisteredUserView

--- app/src/main/java/pt/carrismetropolitana/mobile/composables/screens/home/UnregisteredUserView.kt
+++ app/src/main/java/pt/carrismetropolitana/mobile/composables/screens/home/UnregisteredUserView.kt
@@ -4,31 +4,42 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import pt.carrismetropolitana.mobile.Screens
+import pt.carrismetropolitana.mobile.ui.theme.CMYellow
 
 @Composable
-fun UnregisteredUserView() {
+fun UnregisteredUserView(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround,
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             "A Carris Metropolitana está mais próxima",
             textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Black,
+            fontWeight = FontWeight.ExtraBold,
             fontSize = 24.sp,
             modifier = Modifier.padding(horizontal = 25.dp)
         )
         Text(
-            "Crie uma conta para personalizar a app com as suas linhas e paragens favoritas.",
+            "Personalize a app com as suas linhas e paragens favoritas.",
             modifier = Modifier.padding(horizontal = 12.dp),
             textAlign = TextAlign.Center,
         )
@@ -37,5 +48,20 @@ fun UnregisteredUserView() {
             modifier = Modifier.padding(24.dp),
             textAlign = TextAlign.Center
         )
+
+        TextButton(
+            onClick = {
+                navController.navigate(Screens.FavoritesCustomization.route)
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = CMYellow),
+
+        ) {
+            Text("Adicionar favoritos",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(horizontal = 12.dp)
+            )
+        }
+
+        Text("Boas viagens!", fontSize = 12.sp, color = MaterialTheme.colorScheme.onBackground)
     }
 }

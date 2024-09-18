d502aa89f4e19d6bfe8988f2e2cec27d20ad6bc6
joaodcp <67809513+joaodcp@users.noreply.github.com>
2024-09-18 17:36:51 +0100

    fix: add chevron to FavoriteLineWidget

--- app/src/main/java/pt/carrismetropolitana/mobile/composables/components/internal_widgets/FavoriteLineWidget.kt
+++ app/src/main/java/pt/carrismetropolitana/mobile/composables/components/internal_widgets/FavoriteLineWidget.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +28,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,6 +37,7 @@ import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
 import pt.carrismetropolitana.mobile.LocalLinesManager
 import pt.carrismetropolitana.mobile.LocalVehiclesManager
+import pt.carrismetropolitana.mobile.R
 import pt.carrismetropolitana.mobile.composables.components.Pill
 import pt.carrismetropolitana.mobile.composables.components.maps.PatternMapView
 import pt.carrismetropolitana.mobile.services.cmapi.CMAPI
@@ -83,28 +87,48 @@ fun FavoriteLineWidget(
                 .padding(vertical = 18.dp, horizontal = 12.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            if (line != null) {
-                Pill(text = line.shortName, color = Color(line.color.toColorInt()), textColor = Color(line.textColor.toColorInt()), size = 60)
-            } else {
-                Box(modifier = Modifier
-                    .width(72.dp)
-                    .height(24.dp)
-                    .clip(shape = RoundedCornerShape(50))
-                    .shimmerEffect())
+            Box(modifier = Modifier.weight(1f)) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    if (line != null) {
+                        Pill(
+                            text = line.shortName,
+                            color = Color(line.color.toColorInt()),
+                            textColor = Color(line.textColor.toColorInt()),
+                            size = 60
+                        )
+                    } else {
+                        Box(
+                            modifier = Modifier
+                                .width(72.dp)
+                                .height(24.dp)
+                                .clip(shape = RoundedCornerShape(50))
+                                .shimmerEffect()
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    if (pattern != null) {
+                        Text(
+                            pattern?.headsign ?: "", fontWeight = FontWeight.Bold
+                        )
+                    } else {
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Box(
+                            modifier = Modifier
+                                .height(20.dp)
+                                .clip(shape = RoundedCornerShape(50))
+                                .shimmerEffect()
+                        )
+                    }
+                }
             }
-            Spacer(modifier = Modifier.width(8.dp))
-            if (pattern != null) {
-                Text(
-                    pattern?.headsign ?: "", fontWeight = FontWeight.Bold
-                )
-            } else {
-                Spacer(modifier = Modifier.width(12.dp))
-                Box(modifier = Modifier
-                    .weight(1f)
-                    .height(20.dp)
-                    .clip(shape = RoundedCornerShape(50))
-                    .shimmerEffect())
-            }
+
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.chevron_right),
+                contentDescription = "Chevron Right Icon",
+                tint = Color.Gray,
+            )
         }
 
         HorizontalDivider()
